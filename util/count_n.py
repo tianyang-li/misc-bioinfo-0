110e8cbbfead9d45ac8971d9d599452fed1ac894
Tianyang Li <tmy1018@gmail.com>
2012-11-03 20:47:39 +0800

    fjgeiorgoo

--- util/count_n.py
+++ util/count_n.py
@@ -15,17 +15,78 @@
 #
 #  You should have received a copy of the GNU General Public License
 
+
 """
 count the number of read (or read pair)
 with N in the sequence
 """
 
+# TODO: N distribution
+
+from __future__ import division
+
 import getopt
+import sys
+from itertools import izip
 
 from Bio import SeqIO
 
-def main(argv):
-    opts, _ = getopt.getopt(argv, 's:1:2', [])
+
+def single_count_n(file, fmt):
+    rec_count = 0
+    n_count = 0
     
+    for rec in SeqIO.parse(file, fmt):
+        rec_count += 1
+        if "N" in str(rec.seq).upper():
+            n_count += 1
+            
+    print "%d/%d" % (n_count, rec_count)
+    print n_count / rec_count
+
+
+def paired_count_n(file1, file2, fmt):
+    pair_count = 0
+    n_count = 1
+    
+    for rec1, rec2 in izip(SeqIO.parse(file1, fmt), SeqIO.parse(file2, fmt)):
+        pair_count += 1
+        if ("N" in str(rec1.seq).upper()) or ("N" in str(rec2.seq).upper()):
+            n_count += 1
+            
+    print "%d/%d" % (n_count, pair_count)
+    print n_count / pair_count
+
+
+def main(argv):
+    opts, _ = getopt.getopt(argv, 's:1:2:f:', [])
+
+    single_file = None
+    pair_file_1 , pair_file_2 = None, None
+    fmt = None
+
+    for opt, arg in opts:
+        if opt == '-s':
+            single_file = arg
+        if opt == '-1':
+            pair_file_1 = arg
+        if opt == '-2':
+            pair_file_2 = arg
+        if opt == '-f':
+            fmt = arg
+    
+    if (not single_file 
+        or not pair_file_1
+        or not pair_file_2):
+        print >> sys.stderr, "missing options"
+        sys.exit(1)
+    
+    if single_file:
+        single_count_n(single_file, fmt)
+        return
+    
+    if not pair_file_1 or not pair_file_2:
+        print >> sys.stderr, "missing options"
+        sys.exit(1)
     
 
